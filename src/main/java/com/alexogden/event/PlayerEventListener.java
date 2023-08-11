17224e53d65ef3a549054efa601661d17ee0f171
Alex Ogden <1379601+AlexOgden@users.noreply.github.com>
2023-08-11 17:17:19 +0100

    feat(#1): Add PlayerEventListener to handle auto-pause of BackupTask

---
+++ src/main/java/com/alexogden/event/PlayerEventListener.java
@@ -0,0 +1,46 @@
+package com.alexogden.event;
+
+import com.alexogden.core.SCAutoBackup;
+import com.alexogden.task.ServerTask;
+import org.bukkit.Bukkit;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+
+public class PlayerEventListener implements Listener {
+
+	private final SCAutoBackup plugin;
+
+	public PlayerEventListener(SCAutoBackup plugin) {
+		this.plugin = plugin;
+	}
+
+	@EventHandler
+	public void onPlayerJoin(PlayerJoinEvent event) {
+		ServerTask backupTask = plugin.getBackupTask();
+		boolean isBackupPaused = backupTask.isPaused();
+		boolean isAutoPauseEnabled = plugin.getConfig().getBoolean("backup.pause-on-empty-server");
+
+		if (isBackupPaused && isAutoPauseEnabled) {
+			backupTask.resume();
+			Bukkit.getLogger().info("Auto Backup Resumed!");
+		}
+	}
+
+	@EventHandler
+	public void onPlayerQuit(PlayerQuitEvent event) {
+		Bukkit.getScheduler().runTaskLater(plugin, () -> {
+			ServerTask backupTask = plugin.getBackupTask();
+			boolean isServerEmpty = Bukkit.getServer().getOnlinePlayers().isEmpty();
+			boolean isAutoPauseEnabled = plugin.getConfig().getBoolean("backup.pause-on-empty-server");
+
+			if (isServerEmpty && isAutoPauseEnabled) {
+				Bukkit.getLogger().info("Server is empty, triggering backup and pausing auto backup.");
+				backupTask.run();
+				backupTask.pause();
+			}
+		}, 40L);
+	}
+}
+
